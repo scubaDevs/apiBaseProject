21bd759ed35281bf8b62e76843f243c058211270
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2022-04-18 00:35:48 -0300

    Update pg.ts

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -5,10 +5,11 @@ dotenv.config();
 
 
 export const sequelize = new Sequelize(
-    process.env.PG_URL as string,
-    {
-        
-        dialect: 'postgres',
-        port: parseInt(process.env.PG_PORT as string)
-    }
+    process.env.PG_URL as string
+    //,
+   // {
+   //     
+   //     dialect: 'postgres',
+   //     port: parseInt(process.env.PG_PORT as string)
+   // }
 );
