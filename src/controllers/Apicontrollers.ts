0b19cd11df26a5cb582a16952adbd71d62c623dd
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2022-04-17 22:20:17 -0300

    new

--- src/controllers/Apicontrollers.ts
+++ src/controllers/Apicontrollers.ts
@@ -10,6 +10,3 @@ export const home = (req:Request,res: Response)=>{
     res.json({msg: 'Olá mundo!'})
 };
 
-export const sobre = (req:Request,res:Response)=>{
-    res.send("Esta é a página sobre")
-}
