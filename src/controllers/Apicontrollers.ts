fc986539f0b77897a0ab3a6ea5b7c4c16468c392
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2022-04-20 06:02:50 -0300

    Auth ready!

--- src/controllers/Apicontrollers.ts
+++ src/controllers/Apicontrollers.ts
@@ -1,6 +1,7 @@
 import {Request,Response} from 'express';
 import {User} from "../models/User";
 import {sequelize} from '../instances/pg';
+import { Auth } from '../middlewares/auth';
 
 
 
@@ -19,3 +20,7 @@ export const ping = async (req:Request,res:Response)=>{
 export const home = (req:Request,res:Response)=>{
     res.json({message:'olá mundo novo'})
 }
+
+export const sobre = (req:Request, res:Response)=>{
+    res.json({message: 'Esta é a página sobre!'})
+};
